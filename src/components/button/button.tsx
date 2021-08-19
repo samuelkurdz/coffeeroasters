0e54bcac9a2803fa49838f1b49c2929084142df7
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-08-19 19:12:11 +0100

    hero section in build

---
+++ src/components/button/button.tsx
@@ -0,0 +1,12 @@
+
+import './button.css';
+
+const Button = () => {
+	return (
+		<button>
+			button
+		</button>
+	)
+}
+
+export default Button;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,25 @@
 
 import './Home.css';
+import Button from '../../components/button/button';
 
 const Home = () => {
 	return (
 		<div className="home">
-			<p>Home</p>
+			<section className="hero">
+				<h1 className="hero__big-text">
+					Great coffee made simple.
+				</h1>
+				<h2 className="hero__big-text">
+					Great coffee made simple.
+				</h2>
+				<p className="hero__message">
+					Start your mornings with the world’s best coffees.
+					Try our expertly curated artisan coffees from our
+					best roasters delivered directly to your door, at your schedule.
+				</p>
+
+				{/* <Button /> */}
+			</section>
 		</div>
 	)
 };
