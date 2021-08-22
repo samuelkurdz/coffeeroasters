fe4c0ede4ced9c3180031f73fc94b0a64e1e3ce0
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-08-22 02:07:22 +0100

    how it works

--- src/components/how-it-works/how-it-works.tsx
+++ src/components/how-it-works/how-it-works.tsx
@@ -1,11 +1,46 @@
 
 import './how-it-works.css';
 
+interface CollectionItem {
+	title: string;
+	description: string;
+	header: string;
+}
+
+const items: CollectionItem[] = [
+	{
+		title: '01',
+		header: 'Pick your coffee',
+		description: 'Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.',
+	},
+	{
+		title: '02',
+		header: 'Choose the frequency',
+		description: 'Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.',
+	},
+	{
+
+		title: '03',
+		header: 'Receive and enjoy!',
+		description: 'We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.',
+	},
+];
 
 const HowItWorks = () => {
 	return (
-		<div className="how">
-			<h1>HowItWorks</h1>
+		<div className="howItWorks">
+			<h4>How it works</h4>
+			<div className="howItWorks__container">
+				{
+					items.map(({title, header, description}) => (
+						<div className="howItWorks__item">
+							<h1>{title}</h1>
+							<h3>{header}</h3>
+							<p>{description}</p>
+					</div>
+					))
+				}
+			</div>
 		</div>
 	)
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
 			<CollectionSection />
 
 			<WhyUs />
-			{/* <HowItWorks /> */}
+			<HowItWorks />
 		</div>
 	)
 };
