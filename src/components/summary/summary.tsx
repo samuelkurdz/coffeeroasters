d6540cd77e89b3a2aa79b4eb08a5c10a74810598
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-08-30 01:02:53 +0100

    mini

--- src/components/summary/summary.tsx
+++ src/components/summary/summary.tsx
@@ -3,7 +3,13 @@ import './summary.css';
 const Summary = () => {
 	return (
 		<div className="summary">
-			<p>Summary</p>
+			<h5>ORDER SUMMARY</h5>
+			<p>
+				“I drink my coffee as Espresso,
+				with a Decaf type of bean.
+				1000g ground ala Filter,
+				sent to me Every month.”
+			</p>
 		</div>
 	)
 };
